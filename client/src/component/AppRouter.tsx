efafd579ea1744d84c4eac1e489245faa92e4c21
wz2cool <wz2cool@live.cn>
2019-04-06 18:45:11 +0800

    init

--- client/src/component/AppRouter.tsx
+++ client/src/component/AppRouter.tsx
@@ -10,16 +10,6 @@ const Readme = React.lazy(() => import("../route/dashboard/Readme"));
 export class AppRouter extends React.PureComponent {
   public render() {
     return (
-      // <HashRouter>
-      //   <Link to="/">Home</Link>
-      //   <Link to="/about">about</Link>
-      //   <Link to="/readme">readme</Link>
-      //   <React.Suspense fallback={<div>Loading...</div>}>
-      //     <Route path="/" component={Home} />
-      //     <Route path="/about" component={About} />
-      //     <Route path="/readme" component={Readme} />
-      //   </React.Suspense>
-      // </HashRouter>
       <HashRouter>
         <Layout style={{ minHeight: "100vh" }}>
           <NavBar />
