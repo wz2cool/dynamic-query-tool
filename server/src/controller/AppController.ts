b86005f246cb7ab10bba0c1dab4882faa3db2a00
wz2cool <wz2cool@live.cn>
2019-04-06 19:11:11 +0800

    add swagger

--- server/src/controller/AppController.ts
+++ server/src/controller/AppController.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "../service/AppService";
+import { ApiUseTags } from "@nestjs/swagger";
 
+@ApiUseTags("AppController")
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly appService: AppService;
+
+  constructor(appService: AppService) {
+    this.appService = appService;
+  }
 
   @Get()
-  getHello(): string {
+  public getHello(): string {
     return this.appService.getHello();
   }
 }
