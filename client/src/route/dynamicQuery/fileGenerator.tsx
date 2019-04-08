29d6a95e5acc9fcf29bcf0c7d29f195a13772502
wz2cool <wz2cool@live.cn>
2019-04-08 23:41:02 +0800

    add

--- client/src/route/dynamicQuery/fileGenerator.tsx
+++ client/src/route/dynamicQuery/fileGenerator.tsx
@@ -1,7 +1,88 @@
 import * as React from "react";
+import { Form, Input } from "antd";
+import { FormComponentProps } from "antd/lib/form";
 
-export default class FileGenerator extends React.Component {
+interface FileGeneratorProps extends FormComponentProps {}
+
+interface FileGeneratorState {}
+
+class FileGenerator extends React.Component<
+  FileGeneratorProps,
+  FileGeneratorState
+> {
+  private validateIPAddress = (rule: any, value: any, callback: any): void => {
+    callback();
+  };
+
   public render() {
-    return <div>File Generator</div>;
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 }
+      }
+    };
+    return (
+      <Form {...formItemLayout}>
+        <Form.Item label="IP Address">
+          {getFieldDecorator("ipAddress", {
+            rules: [
+              {
+                required: true,
+                message: `"IP Address" is required`
+              },
+              {
+                validator: this.validateIPAddress
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="Port">
+          {getFieldDecorator("port", {
+            rules: [
+              {
+                required: true,
+                message: `"Port" is required!`
+              },
+              {
+                validator: this.validateIPAddress
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="User">
+          {getFieldDecorator("user", {
+            rules: [
+              {
+                required: true,
+                message: `"User" is required!`
+              },
+              {
+                validator: this.validateIPAddress
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="Password">
+          {getFieldDecorator("password", {
+            rules: [
+              {
+                required: true,
+                message: "Please input your password!"
+              },
+              {
+                validator: this.validateIPAddress
+              }
+            ]
+          })(<Input type="password" />)}
+        </Form.Item>
+      </Form>
+    );
   }
 }
+
+export default Form.create()(FileGenerator);
