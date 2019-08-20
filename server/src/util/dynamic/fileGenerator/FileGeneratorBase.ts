0e2c44aca62eba33767683ea7771c871850f2205
wz2cool <wz2cool@live.cn>
2019-08-20 13:27:38 +0800

    add

--- server/src/util/dynamic/fileGenerator/FileGeneratorBase.ts
+++ server/src/util/dynamic/fileGenerator/FileGeneratorBase.ts
@@ -26,15 +26,27 @@ export abstract class FileGeneratorBase {
     for (const columnInfo of columnInfos) {
       const javaType = this.typeConverter.convert(columnInfo.type);
       const javaProperty = _.camelCase(columnInfo.name.toLowerCase());
+
+      let useProperty: string;
+      if (javaType === "Timestamp") {
+        useProperty = `${javaProperty} == null ? null : new Timestamp(${javaProperty}.getTime())`;
+      } else if (javaType === "Date") {
+        useProperty = `${javaProperty} == null ? null : new Date(${javaProperty}.getTime())`;
+      } else if (javaType === "Time") {
+        useProperty = `${javaProperty} == null ? null : new Time(${javaProperty}.getTime())`;
+      } else {
+        useProperty = javaProperty;
+      }
+
       const startCaseJavaProperty = _.upperFirst(javaProperty);
       result +=
         `${space}public ${javaType} get${startCaseJavaProperty}() {\r\n` +
-        `${space}${space}return ${javaProperty};\r\n` +
+        `${space}${space}return ${useProperty};\r\n` +
         `${space}}\r\n\r\n`;
 
       result +=
         `${space}public void set${startCaseJavaProperty}(${javaType} ${javaProperty}) {\r\n` +
-        `${space}${space}this.${javaProperty} = ${javaProperty};\r\n` +
+        `${space}${space}this.${javaProperty} = ${useProperty};\r\n` +
         `${space}}\r\n\r\n`;
     }
     return result;
